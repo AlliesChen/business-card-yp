6db04907c7124189e6de37fd957996af5e470b96
YPChen <cyphuaenng@pm.me>
2022-03-11 22:16:56 +0800

    Practice using react without cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import MailSVG from "./icon/mail.svg";
+import LinkedinSVG from "./icon/linkedin.svg";
+import TwitterSVG from "./icon/Twitter-Icon.svg";
+import FacebookSVG from "./icon/Facebook-Icon.svg";
+import InstagramSVG from "./icon/Instagram-Icon.svg";
+import GithubSVG from "./icon/GitHub-Icon.svg";
+
+import myPortrait from "./img/portrait.jpg";
+
+function Card() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="portrait">
+        <img src={myPortrait} alt="me" />
+      </div>
+      <div className="info">
+        <h2>CHEN, YuPung</h2>
+        <p>Front-End Engineer</p>
+        <a href="">YuPung.website</a>
+        <div className="contact" data-link="website">
+          <a href="" data-link="mail">
+            <MailSVG />
+            <p>Email</p>
+          </a>
+          <a href="" data-link="linkedin">
+            <LinkedinSVG />
+            <p>Linkedin</p>
+          </a>
+        </div>
+        <section className="article">
+          <h4 className="article--title">About</h4>
+          <p className="content">
+            Hello, I'm YuPung. I learned web development via vocational
+            training. I've skills for making prototype by Figma, and writing
+            HTML/CSS/JavaScript to build web apps. Please visit my portfolio in
+            this resume. I worked as product manager in industries of computer
+            peripheral and a start-up company. I'm familiar in using English and
+            have experience in business writing with customers in Europe. Since
+            meeting programming in my last job, I'm fascinated by it, especially
+            for the wide use of webpages, which makes me to decide to change
+            career.
+          </p>
+        </section>
+        <section className="article">
+          <h4 className="article--title">Interests</h4>
+          <p className="content">
+            Programming, playing music and climbing mountains!
+          </p>
+        </section>
+      </div>
+      <div className="contact" data-link="social">
+        <TwitterSVG />
+        <FacebookSVG />
+        <InstagramSVG />
+        <GithubSVG />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return <Card />;
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpckPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "[name]_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // todo check
+      {
+        test: /\.png|jpg|gif$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpckPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
